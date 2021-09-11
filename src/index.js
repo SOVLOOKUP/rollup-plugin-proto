8e4a9563853dd0536e4b12fbe19243eb7f1d8e29
sovlookup <gonorth@qq.com>
2021-09-11 21:53:09 +0800

    fix return proto object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createFilter, dataToEsm } from '@rollup/pluginutils'
 import schema from 'protocol-buffers-schema'
+import fs from 'fs'
 
 export default function proto(options = {}) {
   const filter = createFilter(options.include, options.exclude)
@@ -9,7 +10,7 @@ export default function proto(options = {}) {
     transform(code, id) {
       if (!filter(id)) return null
       if (id.slice(-6) === '.proto') {
-        const parsed = schema.parse(id)
+        const parsed = schema.parse(fs.readFileSync(id))
         return {
           code: dataToEsm(parsed, {
             preferConst: options.preferConst,
